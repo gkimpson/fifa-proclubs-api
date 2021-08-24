e04e9518049e67b2c04ee59fd9d483378ccf4897
Gavin Kimpson <gkimpson@gmail.com>
2021-08-24 08:32:54 +0100

    added base results table migration - will expand on this later

---
+++ database/migrations/2021_08_24_071832_create_results_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->string('match_id');
+            $table->integer('home_team_id');
+            $table->integer('away_team_id');
+            $table->integer('home_team_goals');
+            $table->integer('away_team_goals');
+            $table->enum('outcome', ['homewin', 'awaywin', 'draw']);
+            $table->timestamp('date');
+            $table->json('properties')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
