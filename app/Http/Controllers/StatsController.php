41c0d8f1cfe091572d2383b973f2d69995e5b844
Gavin Kimpson <gkimpson@gmail.com>
2021-11-12 08:38:16 +0000

    correctly set timeout for curl

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -375,7 +375,7 @@ class StatsController extends Controller
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 10,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
